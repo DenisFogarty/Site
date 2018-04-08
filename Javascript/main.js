d4b2a0a0e238fbdbe239443caa96fc4002477bb2
Denis Fogarty <denis.g.fogarty@gmail.com@gmail.com>
2018-04-08 20:49:11 +0100

    Added JS function to change change background color and set text to suitable contrast color

--- Javascript/main.js
+++ Javascript/main.js
@@ -7,12 +7,20 @@ function mouseout(id) {
 	text = document.getElementById(id).style.color = "black";
 }
 
-function changeContrast(id) {
+function getColors(id) {
 	var elem = document.body;
 	var colorProperty = window.getComputedStyle(elem, null).getPropertyValue("color").toString();
 	var splitColors = colorProperty.match(/(\d+)+/g);
+}
 
-	window.alert(splitColors);
-
-	elem.style.color = "rgb(256, 0, 0)";
+function darken(id, red, green, blue) {
+	var elem = document.getElementById(id);
+	elem.style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")";
+	
+	if(red > 128 || green > 128 || blue > 128) {
+		elem.style.color = "rgb(0, 0, 0)";
+	}
+	else {
+		elem.style.color = "rgb(255, 255, 255)";
+	}
 }
