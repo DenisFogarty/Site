1ebd59fc0dd7288cde13603e9921062922f3c86b
Denis Fogarty <denis.g.fogarty@gmail.com@gmail.com>
2018-04-08 19:19:09 +0100

    Added javascript function which gets RGB values and splits them

--- Javascript/main.js
+++ Javascript/main.js
@@ -8,15 +8,11 @@ function mouseout(id) {
 }
 
 function changeContrast(id) {
-	if(document.body.style.backgroundColor == "rgb(255, 255, 255)") {
-		document.body.style.backgroundColor = "rgb(0, 0, 0)";
-		document.body.style.color = "rgb(255, 255, 255)";
-	}
-	else {
-		document.body.style.backgroundColor = "rgb(255, 255, 255)";
-		document.body.style.color = "rgb(0, 0, 0)";
-	}
-	//document.getElementById(id).style.color = "hsl(0, 0%, 50%)";
-	var textColor = document.body.style.color;
-	window.alert(textColor);
+	var elem = document.body;
+	var colorProperty = window.getComputedStyle(elem, null).getPropertyValue("color").toString();
+	var splitColors = colorProperty.match(/(\d+)+/g);
+
+	window.alert(splitColors);
+
+	elem.style.color = "rgb(256, 0, 0)";
 }
